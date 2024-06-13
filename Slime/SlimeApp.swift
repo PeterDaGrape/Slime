a0301e1bac0fbee3a73e14f8f527529ebdc9e334
PeterDaGrape <petervine1@icloud.com>
2024-06-13 23:23:18 +0100

    Initial Commit

---
+++ Slime/SlimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlimeApp.swift
+//  Slime
+//
+//  Created by Peter Vine on 13/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct SlimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
